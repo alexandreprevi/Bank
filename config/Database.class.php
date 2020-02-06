7782bf79de3c5590b00c10dc2bf42b182fa585a4
Alexandre Previ <alexandre.previ@gmail.com>
2020-02-06 17:28:27 +0100

    added dotenv variables

--- config/Database.class.php
+++ config/Database.class.php
@@ -1,26 +1,34 @@
 <?php
 
-/* require_once '../vendor/autoload.php';
-
+require_once '../vendor/autoload.php';
+/*
 $dotenv = Dotenv\Dotenv::createImmutable('../');
-$dotenv->load(); */
+$dotenv->load();
+ */
 
+ echo 'Hej';
 class Database
 {
-    private $host = "localhost";
+    /* private $host = "localhost";
     private $dbName = "bank";
     private $dbPort = 10011;
     private $username = "root";
-    private $password = "root";
+    private $password = "root"; */
+    private $host;
+    private $dbName;
+    private $dbPort;
+    private $username;
+    private $password;
+    
     private $conn;
 
     public function connect()
     {
-        /* $this->host = $_ENV["DB_HOST"];
+        $this->host = $_ENV["DB_HOST"];
         $this->dbName = $_ENV["DB_DATABASE"];
         $this->dbPort = $_ENV["DB_PORT"];
         $this->username = $_ENV["DB_USER"];
-        $this->password = $_ENV["DB_PASS"]; */
+        $this->password = $_ENV["DB_PASS"];
 
         /* $this->host = getenv("DB_HOST");
         $this->dbName = getenv("DB_DATABASE");
